709d0ce811030a849b52d42b1d4c412c916159f8
EmptyBlue <lyt0112@outlook.com>
2024-11-09 17:03:12 +0800

    ✨ feat: Enhance USD asset manipulation and visualization in 02.py
    Add functionality to manipulate and visualize USD assets.
    Update the file to open different USD files, retrieve properties,
    and modify attributes of a sphere, including doubling its radius
    and extent, and setting its display color.
    Print hierarchy information for multiple USD assets.

--- IsaacSim/LearnUSD/02.py
+++ IsaacSim/LearnUSD/02.py
@@ -1,7 +1,36 @@
-from pxr import Usd, Vt
+from pxr import Usd, Vt, UsdGeom
 
-stage = Usd.Stage.Open('franka_alt_fingers.usd')
+stage = Usd.Stage.Open('asset/HelloWorld.usda')
 
-xform = stage.GetPrimAtPath('/')
+root = stage.GetPrimAtPath('/hello')
+sphere = stage.GetPrimAtPath('/hello/world')
 
-xform.GetPropertyNames()
+print(root.GetPropertyNames())
+print(root.GetProperties())
+print(root.GetAttributes())
+print(root.GetRelationships())
+
+# Double radius
+radiusAttr = sphere.GetAttribute('radius')
+radiusAttr.Set(radiusAttr.Get() * 2)
+
+# Double extent
+extentAttr = sphere.GetAttribute('extent')
+extentAttr.Set(extentAttr.Get() * 2)
+
+# Set display color
+sphereSchema = UsdGeom.Sphere(sphere)
+color = sphereSchema.GetDisplayColorAttr()
+color.Set([(0,0,1)])
+
+# Print the Ball
+print(stage.GetRootLayer().ExportToString())
+
+
+# Print the hierarchy of h1.usd
+stage = Usd.Stage.Open('asset/h1.usd')
+print([x.GetTypeName() for x in Usd.Stage.Traverse(stage)])
+
+# Print the hierarchy of g1.usd
+stage = Usd.Stage.Open('asset/g1.usd')
+print([x.GetTypeName() for x in Usd.Stage.Traverse(stage)])
